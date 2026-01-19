56e436620fcd54cde49adc3bb658b6acd2d55439
Rafiul901 <rafiuldipu2480@gmail.com>
2026-01-19 18:20:48 +0600

    new

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -171,3 +171,28 @@ function cardCounter(card) {
     return count + " Hold";
   }
 }
+
+//Build a shopping list
+
+console.log("Grocery shopping list");
+
+const shoppingList = [];
+
+console.log("It will be nice to have some fruit to eat.");
+
+shoppingList.push("Apples");
+
+function getShoppingListMsg(arr) {
+  return `Current Shopping List: ${arr}`;
+}
+
+console.log(getShoppingListMsg(shoppingList));
+
+shoppingList.push("Grapes");
+console.log(getShoppingListMsg(shoppingList));
+
+console.log("It looks like we need to get some cooking oil.");
+
+shoppingList.unshift("Vegetable Oil");
+console.log(getShoppingListMsg(shoppingList));
+
