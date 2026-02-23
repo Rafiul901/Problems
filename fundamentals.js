1cfab44127731a19c86a466b9ef7eb7d2452369b
Rafiul901 <rafiuldipu2480@gmail.com>
2026-02-23 20:55:44 +0600

    new problem

---
+++ fundamentals.js
@@ -0,0 +1,20 @@
+//Build the Largest Number Finder
+
+function largestOfAll(arr){
+let result =[]
+
+for(let i=0; i<arr.length; i++){
+    let max = arr[i][0];
+
+    for(let j=1; j<arr[i].length; j++){
+        if(arr[i][j]> max){
+            max=arr[i][j]
+        }
+    }
+    result.push(max)
+}
+return result
+}
+console.log(largestOfAll([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]));
+
+
