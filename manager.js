a47273047adbf5c3cd55aafcffac461c8189a217
Rafiul901 <rafiuldipu2480@gmail.com>
2026-03-12 23:08:15 +0600

    build story telling app

--- manager.js
+++ manager.js
@@ -126,6 +126,27 @@ function diffArray(arr1,arr2){
 }
 // console.log(diffArray(["pen", "book"], ["book", "pencil", "notebook"]));
 
+//Is the number Prime
+function sumPrimes(num){
+  if(num<2) return 0;
+
+  function isPrime(n){
+    for(let i=2; i<n; i++){
+      if(n%i ===0){
+        return false;
+      }
+    }
+    return true;
+  }
+  let sum=0;
+  for(let i=2; i<=num; i++){
+    if(isPrime(i)){
+      sum +=i;
+    }
+  }
+  return sum;
+}
+console.log(sumPrimes(10));
 
 
 
